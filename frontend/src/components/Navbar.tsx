ddee0bc8ce488f5babfa1b1bd36e096b483499dd
Alex Gonzalez <galex2003@icloud.com>
2024-11-19 13:15:45 -0500

    initial commit

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,18 @@
+
+function NavButton() {
+    return(
+        <>
+        </>
+    )
+}
+function Navbar() {
+    return (
+        <>
+        <div className="bg-slate-50 flex">
+            <img src="https://fastly.picsum.photos/id/67/100/50.jpg?hmac=YEUt8VWSewiZxnNZwqdLSKuZTECf10qTrPZiQ8qcGLU"></img>
+        </div>
+        </>
+    )
+}
+
+export default Navbar
