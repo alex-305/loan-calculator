c0dc345d58da05d65731340b45b0085bf7b258a2
Alex Gonzalez <galex2003@icloud.com>
2024-11-20 10:55:04 -0500

    Fixed code styling

--- frontend/src/components/Nav/Navbar.tsx
+++ frontend/src/components/Nav/Navbar.tsx
@@ -5,20 +5,20 @@ import ServicesDropdown from "./ServicesDropdown"
 
 function Navbar() {
     return (
-        <NavigationMenu className="w-full max-w-screen-xl mx-auto border-b-2 flex items-center justify-between px-4">
-          <a href="/" className="mx-3 text-lg">Lorem Ipsum Services</a>
-          <div className="flex-grow flex justify-center">
-            <NavigationMenuList className="flex">
-              <NavItem title="Service">
-                <ServicesDropdown/>
-              </NavItem>
-              <NavItem title="About" href="/about"></NavItem>
-              <NavItem title="Careers" href="/careers"></NavItem>
-              <NavItem title="Vendors Program" href="/vendors-program"></NavItem>
-            </NavigationMenuList>
-          </div>
-          <Button className="h-8 my-1">Get a quote</Button>
-      </NavigationMenu>
+      <NavigationMenu className="w-full max-w-screen-xl mx-auto border-b-2 flex items-center justify-between px-4">
+        <a href="/" className="mx-3 text-lg">Lorem Ipsum Services</a>
+        <div className="flex-grow flex justify-center">
+          <NavigationMenuList className="flex">
+            <NavItem title="Service">
+              <ServicesDropdown/>
+            </NavItem>
+            <NavItem title="About" href="/about"></NavItem>
+            <NavItem title="Careers" href="/careers"></NavItem>
+            <NavItem title="Vendors Program" href="/vendors-program"></NavItem>
+          </NavigationMenuList>
+        </div>
+      <Button className="h-8 my-1">Get a quote</Button>
+    </NavigationMenu>
     )
 }
 
